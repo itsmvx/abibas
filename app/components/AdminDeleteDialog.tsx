a2a68fc80bacc063a7910f902e1f92d84e780224
Vx16 <132477830+itsmvx@users.noreply.github.com>
2024-01-22 23:45:35 +0700

    Update AdminDeleteDialog.tsx

--- app/components/AdminDeleteDialog.tsx
+++ app/components/AdminDeleteDialog.tsx
@@ -1,48 +1,48 @@
-'use client';
-import {
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Modal, ModalBody, ModalCloseButton, ModalContent,
-    ModalFooter, ModalHeader,
-    ModalOverlay,
-} from "@chakra-ui/react";
-import { useRef, useState } from "react";
+// 'use client';
+// import {
+//     Button,
+//     FormControl,
+//     FormLabel,
+//     Input,
+//     Modal, ModalBody, ModalCloseButton, ModalContent,
+//     ModalFooter, ModalHeader,
+//     ModalOverlay,
+// } from "@chakra-ui/react";
+// import { useRef, useState } from "react";
 
-export const AdminDeleteDialog = ({ id, model, isVisible }: { id: string, model: string, isVisible: boolean }) => {
-    const [ isOpen, setIsOpen ] = useState<boolean>(isVisible);
-    const initialRef = useRef(null);
-    return (
-        <>
-            <Modal
-                initialFocusRef={initialRef}
-                isOpen={isOpen}
-                onClose={() => {
-                    setIsOpen(!isOpen);
-                }}
-            >
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Create your account</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                        <FormControl>
-                            <FormLabel>Type <strong className="font-bold">Misuzu sayang</strong><span> below to confirm your action</span></FormLabel>
-                            <Input ref={initialRef} placeholder='Misuzu sayang' />
-                        </FormControl>
-                    </ModalBody>
+// export const AdminDeleteDialog = ({ id, model, isVisible }: { id: string, model: string, isVisible: boolean }) => {
+//     const [ isOpen, setIsOpen ] = useState<boolean>(isVisible);
+//     const initialRef = useRef(null);
+//     return (
+//         <>
+//             <Modal
+//                 initialFocusRef={initialRef}
+//                 isOpen={isOpen}
+//                 onClose={() => {
+//                     setIsOpen(!isOpen);
+//                 }}
+//             >
+//                 <ModalOverlay />
+//                 <ModalContent>
+//                     <ModalHeader>Create your account</ModalHeader>
+//                     <ModalCloseButton />
+//                     <ModalBody pb={6}>
+//                         <FormControl>
+//                             <FormLabel>Type <strong className="font-bold">Misuzu sayang</strong><span> below to confirm your action</span></FormLabel>
+//                             <Input ref={initialRef} placeholder='Misuzu sayang' />
+//                         </FormControl>
+//                     </ModalBody>
 
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3}>
-                            Save
-                        </Button>
-                        <Button onClick={() => {
-                            setIsOpen(!isOpen);
-                        }}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    );
-};
+//                     <ModalFooter>
+//                         <Button colorScheme='blue' mr={3}>
+//                             Save
+//                         </Button>
+//                         <Button onClick={() => {
+//                             setIsOpen(!isOpen);
+//                         }}>Cancel</Button>
+//                     </ModalFooter>
+//                 </ModalContent>
+//             </Modal>
+//         </>
+//     );
+// };
